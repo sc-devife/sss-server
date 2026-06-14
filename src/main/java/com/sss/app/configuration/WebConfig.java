805f7df9f462b93cbba5622097b4e978d85f7e0f
sc-devife <sc.devlife@gmail.com>
2026-06-15 00:54:18 +0530

    fix cors temp

--- src/main/java/com/sss/app/configuration/WebConfig.java
+++ src/main/java/com/sss/app/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://your-production-domain.com")
+                        .allowedOrigins("http://localhost:3000", "http://13.51.169.84")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
